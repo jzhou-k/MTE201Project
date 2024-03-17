ecdb6c2368fff37524be8c9b1b70158567b844a9
Julia Zhou <99359090+jzhou-k@users.noreply.github.com>
2024-03-17 19:18:26 -0400

    Update StatsProject.ino

--- StatsProject.ino
+++ StatsProject.ino
@@ -1,157 +1,152 @@
 #include <Stepper.h>
 
-// Rotary Encoder Inputs
-#define CLK_PIN 3 //pin number reading from encoder clk : signals when rotations occur
-#define DT_PIN 4 // pin number determining direction 
-#define ENCODER_CLICKS_PER_REV 20
-#define BUTTON_PIN 12
-#define DIRECTION_CW 0   // clockwise direction
-#define DIRECTION_CCW 1  // counter-clockwise direction
+#define CLK_PIN 3  //pin number reading from encoder clk : signals when rotations occur
+//#define DT_PIN 4   // pin number determining direction
+//#define BUTTON_PIN 4
+const int PULSES_PER_REV = 128;
+volatile int pulses = 0;
 
-// chang
-// Rotary encoder constants
-const int STEPS_PER_REVOLUTION = 2048;
-const int START_DIST = 300; 
-const int TOTAL_RACK_LENGTH = 1500; 
-const int PITCH_MM = 2; 
-const int TEETH_NUM = 18; 
-const int MAX_MEASURMENT = 12; 
-const int PULSES_PER_REV = 128; 
+const int fsrPin = 0;  // the FSR and 10K pulldown are connected to a0
+int fsrReading;        // the analog reading from the FSR resistor divider
+int fsrReadingAna;
 
-//Encoder variables
-Stepper myStepper = Stepper(STEPS_PER_REVOLUTION, 8, 10, 9, 11);
-int click_counter = 0;
-int currentStateCLK;
-int lastStateCLK;
-int currentDir = DIRECTION_CW;
-volatile int pulses = 0; 
+const int stepsPerRevolution = 64;  // change this to fit the number of steps per revolution
+// for your motor
+volatile long temp, counter = 0;  //This variable will increase or decrease depending on the rotation of encoder
 
-/*
-encoder count 1 / 128 rev * 18 * 2mm = distance traveld 
-displacement = distance traveled * 100 - 3 
-maxMeasurement = totalRackLength - startingCm 
-objectLength = maxMeasurement - displacement  */
+int a, b;
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
-//Touch sensor
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
-const int analogOutPin = 9;  // Analog output pin that the LED is attached to
-
-int sensorValue = 0;  // value read from the pot
-int outputValue = 0;  // value output to the PWM (analog out)
-int ini_sensorValue = 0;
-
-// Calibration
-unsigned long lastButtonPress = 0;
-// TODO: initial dist?
-
-void count2() {
-  /*Reads encoder pulses, 
-    Assumes clockwise is the positive direction when counting pulses
-  */
-  currentStateCLK = digitalRead(CLK_PIN);
-
-  //If the state has changed, it indicates that it has finished a revolution
-  if ( currentStateCLK != lastStateCLK &&  currentStateCLK == HIGH) {
-    if (digitalRead(DT_PIN) == HIGH) {
-      click_counter--;
-      currentDir = DIRECTION_CCW;
-    } else {
-      // the encoder is rotating in clockwise direction => increase the counter
-      click_counter++;
-      currentDir = DIRECTION_CW;
-    }
-
-    Serial.print("DIRECTION: ");
-    if (currentDir == DIRECTION_CW)
-      Serial.print("Clockwise");
-      Serial.println(click_counter);
-    else
-      Serial.print("Counter-clockwise");
-      Serial.println(click_counter);
-  }
-  // save last CLK state
-  lastStateCLK = currentStateCLK;
-}
-
-
-
-void count_encoder_by_interrupt() { 
-  if (digitalRead(CLK)) { 
-    pulses++; 
-  } else { 
-    pulses--; 
+void count_encoder_by_interrupt() {
+  if (digitalRead(CLK_PIN)) {
+    pulses++;
   }
 }
 
-// returns distance rack traveled by mm 
-unsigned long calculateRackDistance(int pulse) { 
-  return ( (float)pulse / (float)pulseperturn ) * 59.69 ; 
+float calculateRackDistance(int pulse) {
+  //return ((float)pulse / (float)23.2) * 10;
+  return ( (float)pulse / (float)PULSES_PER_REV ) * 51 ;
 }
 
-int calculateDisplacement(int pulse){ 
-  return totalRackLength - calculateRackDistance(pulse); 
+float calculateDimension(int pulse) {
+  float traveled = calculateRackDistance(pulse);
+  return  100 - traveled;
 }
+
+int min_value_ana = 0;
+int max_value_ana = 0;
+int max_value;
+int tolerance_val = 1;
+
+// constants won't change. They're used here to set pin numbers:
+const int BUTTON_PIN = 7; // the number of the pushbutton pin
+
+// Variables will change:
+int lastState = HIGH; // the previous state from the input pin
+int currentState;    // the current reading from the input pin
+
+
 
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(CLK_PIN, INPUT); 
-  pinMode(DT_PIN, INPUT);
+
+  pinMode(2, INPUT_PULLUP);  // internal pullup input pin 2
+  pinMode(3, INPUT_PULLUP);  // internalเป็น pullup input pin 3
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  attachInterrupt(0, count_encoder_by_interrupt, RISING); 
+  //Setting up interrupt
+  //A rising pulse from encodenren activated ai0(). AttachInterrupt 0 is DigitalPin nr 2 on moust Arduino.
+  attachInterrupt((0), ai0, RISING);
+
+  //B rising pulse from encodenren activated ai1(). AttachInterrupt 1 is DigitalPin nr 3 on moust Arduino.
+  attachInterrupt((1), ai1, RISING);
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(30);
+  // initialize the serial port:
   Serial.begin(9600);
-
-  lastStateCLK = digitalRead(CLK_PIN);
-  myStepper.setSpeed(10);
 }
 
-
-void measure() { 
-    myStepper.step(8);
-    Serial.print("pulses = "); 
-    Serial.println(pulses); 
-    Serial.print("rack distance traveled = "); 
-    Serial.println(calculateRackDistance(pulses)); 
-
-    delay(1000);
-}
+int min_value;
 
 
-bool pressureDetected() { 
-  sensorValue = analogRead(analogInPin);
-  return (sensorValue > 5); 
-}
-
-
-// the loop function runs over and over again forever
 void loop() {
-  // digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  // step one revolution  in one direction:
 
-  digitalWrite(LED_BUILTIN, HIGH); // LED on, motor rotating backwards 
+  max_value_ana = analogRead(fsrPin);
+
+  // Serial.println(max_value_ana);
+  // Serial.println("adjust motor");
+  // delay(5000);
+  //currentState = digitalRead(BUTTON_PIN);
+
+  counter = 0;
+  //min_value = fsrReading = map(min_value_ana, 0, 1023, 0, 255);
+  currentState = digitalRead(BUTTON_PIN);
+
+  if(lastState == LOW && currentState == HIGH)
+    Serial.println("The state changed from LOW to HIGH");
+  fsrReadingAna = 0;
   while (digitalRead(BUTTON_PIN)) {
-    myStepper.step(-12);
-    delay(10);
-  }
- 
-  digitalWrite(LED_BUILTIN, LOW); //LED off, motor rotating forwards 
+    //Serial.println("clockwise");
+    //myStepper.step(12);
 
-  pulses = 0;  
-  while (!pressureDetected()) {
-    measure(); 
-    count2();
+    // fsrReadingAna = analogRead(fsrPin);
+    // //fsrReading = map(fsrReadingAna, 0, 1023, 0, 255);
+    // Serial.print("Analog reading = ");
+    // Serial.print(fsrReadingAna);     // print the raw analog reading
+
+    // if (fsrReadingAna <= max_value_ana + tolerance_val) {
+    //   Serial.println(" - No pressure");
+    //   Serial.println("max value = ");
+    //   Serial.print(max_value_ana);
+    // } else {
+    //   Serial.println(" - Big squeeze");
+    // }
+    if (counter != temp) {
+      Serial.println(counter);
+      temp = counter;
+    }
+    Serial.print("Distance: ");
+    Serial.print(calculateDimension(counter));
+    Serial.print("   Encoder: ");
+    Serial.println(counter);
+    delay(50);
   }
 
-  Serial.println("------------------------------------")  
-  Serial.println("MEASURING FINISHED") 
-  Serial.println("LENGTH OF THE OBJECT IS: ")
-  Serial.print(objectLength); 
+  Serial.println("------------------------------------");
+  Serial.println("MEASURING FINISHED");
+  Serial.print("LENGTH OF THE OBJECT IS: ");
+  Serial.print(calculateDimension(counter));
+  Serial.print("mm");
+  delay(5000);
+  lastState = currentState;
 
-  Serial.println("PRESS BUTTON AGAIN TO REVERT THE RACK AND MEASURE AGAIN") 
-  while (digitalRead(BUTTON_PIN)) { 
-      //waiting for user button press 
-  }
+  // delay(10);
 }
 
 
 
+void ai0() {
+  // ai0 is activated if DigitalPin nr 2 is going from LOW to HIGH
+  // Check pin 3 to determine the direction
+  // (a == HIGH && a == b) || (a == LOW && a == b)
+  //
+  a = digitalRead(3);
+  b = digitalRead(2);
+  if ((a == HIGH && a == b) || (a == LOW && a == b)) {
+    counter++;
+  } else {
+    //counter--;
+  }
+}
 
+void ai1() {
+  // ai0 is activated if DigitalPin nr 3 is going from LOW to HIGH
+  // Check with pin 2 to determine the direction
+  a = digitalRead(3);
+  b = digitalRead(2);
+  if (a != b) {
+    //counter--;
+  } else {
+    counter++;
+  }
+}
